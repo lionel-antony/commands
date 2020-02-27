731ef5fac9e9084075282e87f8d8d8b8c51d9b44
Lionel Antony <antonyl@frontiersoftware.co.in>
2020-02-27 16:07:28 +0530

    Upd

---
+++ ps/ps_com_01.ps1
@@ -0,0 +1,15 @@
+$psversiontable - show version
+
+Get-Module -ListAvailable
+
+Choco outdated
+Choco upgrade powershell-core
+
+Get-NetIPConfiguration
+
+New-NwtIPAddress -InterfaceAlias Ethernet -IPAddress 172.16.0.20 -PrefixLength 24 -DefaultGateway 172.16.0.1
+
+Get-Host
+
+Get-Process | FL 
+Get-Process | FT
