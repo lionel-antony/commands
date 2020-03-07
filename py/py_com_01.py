86308c150dedc7d4ef95dd8b8bf8a1890f37077c
Lionel Antony <lionel.antony@gmail.com>
2020-03-07 21:52:40 +0530

    upd

--- py/py_com_01.py
+++ py/py_com_01.py
@@ -9,3 +9,21 @@ print(name)
 print()
 print('Blank line \nin the middle of string')
 print('Hello \nWorld')
+
+# Use dobule quotes when a single quote is in the string
+print("It's a small world.")
+
+# Strings can be stored in variables
+first_name = 'Christopher'
+print(first_name)
+
+first_name = 'Lionel'
+last_name = 'Antony'
+print('Hello ' + first_name + ' ' + last_name)
+
+# Use functions to modify strings
+sentence = 'The bird is kingfisher'
+print(sentence.upper())
+print(sentence.lower())
+print(sentence.capitalize())
+print(sentence.count('a'))
