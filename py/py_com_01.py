7719ad672d6eccb46236dd3c47db32bbdc2e6450
Lionel Antony <lionel.antony@gmail.com>
2020-03-07 20:55:02 +0530

    upd

---
+++ py/py_com_01.py
@@ -0,0 +1,11 @@
+# Print info
+print('Hello World')
+
+# Getting info from the user
+name = input('Please enter your name: ')
+print(name)
+
+# Blank line
+print()
+print('Blank line \nin the middle of string')
+print('Hello \nWorld')
