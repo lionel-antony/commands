ea98f1d5ce502ac60bde180b8e8094b50364adc9
Lionel Antony <lionel.antony@gmail.com>
2020-03-23 22:18:33 +0530

    upd

---
+++ lux/ssh01.sh
@@ -0,0 +1,12 @@
+# install ssh server
+
+sudo apt install openssh-Server
+
+# login from another machine
+
+ssh lja@111.222.333.444
+
+scp test.txt 192.168.111.222:~/Desktop/test.txt
+
+# ssh using GUI
+ssh -X lja@111.222.333.444
