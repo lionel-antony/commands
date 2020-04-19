eeaafd31beaa271c9b4467b1aae649d3e71d6a36
Lionel Antony <antonyl@frontiersoftware.co.in>
2020-04-19 15:51:11 +0530

    upd

--- docker/dock02.sh
+++ docker/dock02.sh
@@ -109,4 +109,110 @@ docker network connect
 # detach a network from a container
 docker network disconnect
 
+### create network - default is bridge ###
+docker network create lio
+docker network create jef
 
+docker container run -d -p 8080:80 --network lio --name lionginx nginx
+docker container run -d -p 8090:80 --network jef --name jefnginx nginx
+
+docker container run -d -p 10:80 --network lio --name lioapache httpd
+docker container run -d -p 20:80 --network jef --name jefapache httpd
+
+docker container run -d -p 8181:8081 --network lio --name liomongo mongo
+docker container run -d -p 8281:8081 --network jef --name jefmongo mongo
+
+docker network inspect lio
+docker network inspect jef
+
+# disconnect liomysql from lio network and connect to jef network
+# disconnect jefmysql from jef network and connect to lio network
+docker network disconnect a1c7dac34842  e6c6c552f1e8
+docker network disconnect 5893544919ab 3bef457cb101
+docker network connect a1c7dac34842 3bef457cb101
+docker network connect 5893544919ab e6c6c552f1e8
+
+# connect liomysql to lio and  jefmysql to jef
+docker network connect a1c7dac34842 e6c6c552f1e8
+docker network connect 5893544919ab 3bef457cb101
+
+        "Containers": {
+            "3bef457cb1015c0f2286ecebc46824ff1c73e8bd03e7c699be27f1e355fc10d6": {
+                "Name": "jefmongo",
+                "EndpointID": "fa73ada54fa004d67e7e55a32442fc754a25a5ada4c1ca7299431edd0f41126e",
+                "MacAddress": "02:42:ac:14:00:04",
+                "IPv4Address": "172.20.0.4/16",
+                "IPv6Address": ""
+            },
+            "40b09d9f93c01f7fe6366bdbd9ee9d99cde56f3a7499eacdb80189d4c04fd743": {
+                "Name": "lioapache",
+                "EndpointID": "b991591057d642de24ced86d9bc2c377902b1af518302370144ab828e45b09eb",
+                "MacAddress": "02:42:ac:14:00:03",
+                "IPv4Address": "172.20.0.3/16",
+                "IPv6Address": ""
+            },
+            "89b0eca5ef4419316c7fe113825164d6be65f83fdcea9bd8ec33cf2d07b9eb39": {
+                "Name": "lionginx",
+                "EndpointID": "f6b409acf7bf1dab4f76266bcaf5ba227c373c3dbcde8bf107ab317a69c92a3d",
+                "MacAddress": "02:42:ac:14:00:02",
+                "IPv4Address": "172.20.0.2/16",
+                "IPv6Address": ""
+            },
+            "e6c6c552f1e82765cd09490a75a27ac25e59af228dc40cd8e7ceee9d7697abd8": {
+                "Name": "liomongo",
+                "EndpointID": "9e1ca5c5ba44d233c6d0228829fec1829d13f1afaa21cfc62b3950a35c456f0a",
+                "MacAddress": "02:42:ac:14:00:05",
+                "IPv4Address": "172.20.0.5/16",
+                "IPv6Address": ""
+            }
+
+        "Containers": {
+            "3bef457cb1015c0f2286ecebc46824ff1c73e8bd03e7c699be27f1e355fc10d6": {
+                "Name": "jefmongo",
+                "EndpointID": "fa39fcb01f222f11a2ea5445eef8c9f913d7d64bc364fe099f84c7649af99775",
+                "MacAddress": "02:42:ac:15:00:05",
+                "IPv4Address": "172.21.0.5/16",
+                "IPv6Address": ""
+            },
+            "52b51729349d138af49f17aaca8f77f40926b9202f4337fd6af2177710e79fb9": {
+                "Name": "jefapache",
+                "EndpointID": "de46fb4e26a71c5ab3eb7901c0e5689d0a7c25d4f9f864e266a1d5b281874dbc",
+                "MacAddress": "02:42:ac:15:00:03",
+                "IPv4Address": "172.21.0.3/16",
+                "IPv6Address": ""
+            },
+            "8590938aa7bb4485ecceb42f7da902f83cda2d2d5d5db416c16ac17b41f382e8": {
+                "Name": "jefnginx",
+                "EndpointID": "51752e64ddb01b210b5735e450a5537e868e141720c997d71548d800beb78a7d",
+                "MacAddress": "02:42:ac:15:00:02",
+                "IPv4Address": "172.21.0.2/16",
+                "IPv6Address": ""
+            },
+            "e6c6c552f1e82765cd09490a75a27ac25e59af228dc40cd8e7ceee9d7697abd8": {
+                "Name": "liomongo",
+                "EndpointID": "46c1c1690e97f6777ecc5ad98f208313d31d18fa9dbf00d5ed282ffc7e9223a9",
+                "MacAddress": "02:42:ac:15:00:04",
+                "IPv4Address": "172.21.0.4/16",
+                "IPv6Address": ""
+            }
+        },
+
+### create network - default is bridge ###
+
+### Docker Networks: DNS ###
+# Docker uses Container names as the equivalent of host names when talking to each other
+# DNS is the key into easy inter-container communication
+# DNS for names is built-in for custom networks
+
+# ping containers on the same network
+docker container exec -it lionginx ping liomongo
+
+# Bridge network does not have the DNS Server built into it by default
+# -- link options is used to create manual links between containers
+
+### End - Docker Networks: DNS ###
+
+### install ping ###
+apt-get update
+apt-get install iputils-ping
+### end - install ping ###
