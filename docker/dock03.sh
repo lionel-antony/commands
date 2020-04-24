e7ac9f2e51bc409ef351c9fda9c079daaa3e941e
Lionel Antony <lionel.antony@gmail.com>
2020-04-24 21:31:31 +0530

    upd

---
+++ docker/dock03.sh
@@ -0,0 +1,18 @@
+# Docker DevOps Course - KodeKloud
+
+# check linux Version
+cat /etc/*release*
+
+# install docker from get.docker.com
+curl -fsSL https://test.docker.com -o test-docker.sh
+sudo sh test-docker.sh
+
+# docker version
+sudo docker version
+
+# test an image
+docker pull docker/whalesay
+docker run docker/whalesay cowsay boo
+
+# docker run 
+
