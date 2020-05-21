0cfb9cc46ebbc991775100ad620e5bdc5c64b470
Lionel Antony <lionel.antony@gmail.com>
2020-05-21 17:46:05 +0530

    upd

---
+++ ps/ps_font_01.ps1
@@ -0,0 +1,15 @@
+# Install oh-my-posh
+
+
+Install-Module posh-git -Scope CurrentUser
+Install-Module oh-my-posh -Scope CurrentUser
+Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentUser -Force -SkipPublisherCheck
+
+# run "notepad $PROFILE" and add these lines to the end:
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-Theme Paradox
+
+# font face 
+# "fontFace":  "Cascadia Code PL"
+
